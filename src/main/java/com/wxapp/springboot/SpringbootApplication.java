3e3adbaf9c2f77a672b318cb0d93091b3f85056d
付铭 <fuming.137@icloud.com>
2020-05-07 15:08:24 +0800

    update main

--- src/main/java/com/wxapp/springboot/SpringbootApplication.java
+++ src/main/java/com/wxapp/springboot/SpringbootApplication.java
@@ -1,13 +1,20 @@
 package com.wxapp.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //2020年微信小程序大赛小程序作品服务端
+//
 @SpringBootApplication
 public class SpringbootApplication {
     public static void main(String[] args) {
+        TomcatConfig TCC=new TomcatConfig();
         SpringApplication.run(SpringbootApplication.class, args);
-        System.out.println("Start success!Server listening on https://127.0.0.1:443");
+        TCC.webServerFactory();
+        Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+        logger.info("Server listening on https://127.0.0.1:443");
+        //System.out.println("Start success!Server listening on https://127.0.0.1:443");
     }
 }
